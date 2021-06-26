433a7e96428ad792bfa9f82a82a1bd68d415ebad
Mathieu <mathieu_fretiere@yahoo.fr>
2021-06-26 11:11:34 +0200

    version 1

---
+++ src/main/java/com/mycompany/testnetbeans124/Test.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testnetbeans124;
+
+import java.util.*;
+
+/**
+ *
+ * @author Mathieu
+ */
+public class Test {
+    
+    public static void main(String[] args)
+    {
+        System.err.println("Bonjour");
+        
+        Date date;
+        
+        date = new Date(2000, 12, 1);
+        
+        String s = new String("bonjour");
+        
+        Integer entier = 10;
+        
+        //Integer.parseInt()
+        //entier.
+        
+        System.err.println("Au revoir");
+    }
+    
+}
